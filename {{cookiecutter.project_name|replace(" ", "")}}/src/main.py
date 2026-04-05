801067021d186fef1362dcb53afd0f9edf51c83c
JonZeolla <jon@zenable.io>
2026-04-05 11:47:51 -0400

    fix(ci): clean up CI warnings and deprecations (#91)

--- {{cookiecutter.project_name|replace(" ", "")}}/src/main.py
+++ {{cookiecutter.project_name|replace(" ", "")}}/src/main.py
@@ -10,10 +10,11 @@ from {{ cookiecutter.project_slug }} import __version__, config
 
 def main():
     """Main entry point for the application."""
-    parser = argparse.ArgumentParser(description="{{ cookiecutter.project_short_description | replace('"', '\\"') | replace("'", "\\\\'") }}")
-    parser.add_argument(
-        "--version", action="version", version=f"%(prog)s {__version__}"
+    parser = argparse.ArgumentParser(
+        prog="{{ cookiecutter.project_slug }}",
+        description="{{ cookiecutter.project_short_description | replace('"', '\\"') | replace("'", "\\\\'") }}",
     )
+    parser.add_argument("--version", action="version", version=__version__)
     parser.parse_args()
 
     log = config.setup_logging()
